f2f3e3b69ce604fc488a4cdcb0e57094ee7e7113
pavva91 <valerio.mattioli@proton.me>
2024-02-14 10:52:43 +0100

    add italian

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,7 +1,8 @@
 import en from './translations/en.json'
 import fr from './translations/fr.json'
 import nl from './translations/nl.json'
+import it from './translations/it.json'
 
 export default {
-    en, fr, nl
+    en, fr, nl, it
 }
