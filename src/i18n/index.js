c2e8bfab48d4010cb9a1e8addad072941450b261
pavva91 <valerio.mattioli@proton.me>
2024-08-27 14:02:15 +0200

    fix language commons with fallbackLocale 'en'

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,9 +2,11 @@ import { createI18n } from 'vue-i18n'
 import messages from '../lang'
 
 export default createI18n({
-    locale: import.meta.env.VITE_DEFAULT_LOCALE,
-    fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
-    legacy: false,
-    globalInjection: true,
-    messages,
+  // locale: 'en',
+  // fallbackLocale: 'en',
+  locale: import.meta.env.VITE_DEFAULT_LOCALE,
+  fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
+  legacy: false,
+  globalInjection: true,
+  messages
 })
