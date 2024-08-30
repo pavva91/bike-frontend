83ab75df9187d426b1620bbbc5e99338e6ed721f
pavva91 <valerio.mattioli@proton.me>
2024-08-30 11:10:25 +0200

    menu desktop, fix submenu detached

--- src/components/MenuDesktop.vue
+++ src/components/MenuDesktop.vue
@@ -10,7 +10,7 @@ const hover = ref(false)
   <nav class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="@/assets/menu_logo.png" class="h-8" alt="Flowbite Logo" />
+        <img src="@/assets/menu_logo.png" class="h-8" alt="Enterprise Logo" />
         <!-- TODO: My company name -->
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{
           $t('company')
@@ -21,7 +21,7 @@ const hover = ref(false)
         id="navbar-language"
       >
         <ul
-          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
           <li>
             <RouterLink :to="{ name: 'Home' }">{{ $t('nav.home') }}</RouterLink>
@@ -34,7 +34,7 @@ const hover = ref(false)
           </li>
           <li @mouseover="hover = true" @mouseleave="hover = false">
             {{ $t('nav.tours') }}
-            <ul v-if="hover">
+            <ul class="sub-menu bg-white" v-if="hover">
               <li>
                 <RouterLink :to="{ name: 'Tours', params: { type: 'road', id: 1 } }"
                   >{{ $t('nav.types.road') }}
@@ -69,3 +69,12 @@ const hover = ref(false)
     </div>
   </nav>
 </template>
+<style>
+.sub-menu {
+  position: absolute;
+  top: 60%;
+}
+.sub-menu li {
+  padding: 10px;
+}
+</style>
