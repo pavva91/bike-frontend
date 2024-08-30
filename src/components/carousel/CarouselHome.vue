fff8b330b37d389dfc91dfa4d38d3ca6b362e23b
pavva91 <valerio.mattioli@proton.me>
2024-08-30 14:46:53 +0200

    organize components for reusability

---
+++ src/components/carousel/CarouselHome.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { FwbCarousel } from 'flowbite-vue'
+
+const pictures = [
+  { src: '/img/11978_cucine-lube-agnese-style-7-1.webp', alt: 'Image 1' },
+  { src: '/img/tours/road/road1.jpg', alt: 'Image 1' },
+  { src: '/img/tours/gravel/gravel1.jpg', alt: 'Image 2' },
+  { src: '/img/tours/mtb/mtb1.jpg', alt: 'Image 3' }
+]
+</script>
+
+<template>
+  <fwb-carousel
+    style="height: 200px"
+    class="relative h-screen"
+    :pictures="pictures"
+    slide
+    :slide-interval="10000"
+  />
+</template>
+
+<style></style>
---
+++ src/components/footer/FooterInfo.vue
@@ -0,0 +1,70 @@
+<template>
+  <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <div class="footer-divs">
+      <div
+        class="w-full max-w-screen-xl mx-auto p-4 md:py-8 sm:flex sm:items-center sm:justify-between"
+      >
+        <div>
+          <img src="@/assets/bikeservice.png" class="h-8" alt="Enterprise Logo" />
+        </div>
+
+        <div>
+          <p>BIKE SERVICE CORINALDO<br />VIA NEVOLA, 5<br />60013 CORINALDO (AN)</p>
+        </div>
+
+        <div>
+          <p>
+            <a href="mailto:bikeservicecorinaldo@gmail.com">bikeservicecorinaldo@gmail.com</a
+            ><br />T: +39 071 67 697
+          </p>
+        </div>
+
+        <div>
+          <div>
+            <a
+              href="https://www.iubenda.com/privacy-policy/99051779/cookie-policy"
+              class="iubenda-white no-brand iubenda-noiframe iubenda-embed iubenda-noiframe"
+              title="Cookie Policy "
+              >Cookie Policy</a
+            >
+          </div>
+          <div>
+            <a
+              href="https://www.iubenda.com/privacy-policy/99051779"
+              class="iubenda-white no-brand iubenda-noiframe iubenda-embed iubenda-noiframe"
+              title="Privacy Policy "
+              >Privacy Policy</a
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-divs">
+        <div>
+          <p><strong>© Bike Service |&nbsp;</strong>P.Iva 02803080429</p>
+        </div>
+        <div>
+          <a href="#">
+            <img
+              src="https://bikeservicecorinaldo.it/wp-content/uploads/2023/11/firmasitiTDS.png"
+              class="h-8"
+              alt="Enterprise Logo"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<style>
+.footer-divs div {
+  padding: 0.5rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.footer-divs img {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
