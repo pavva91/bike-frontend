1a4185c2f2e8e5f20e817fc83905e25ae7b6eeee
pavva91 <valerio.mattioli@proton.me>
2024-11-09 15:26:35 +0100

    clean mobile navbar

--- src/components/MenuMobile.vue
+++ src/components/MenuMobile.vue
@@ -17,34 +17,36 @@ const showMenu = ref(false)
           $t('company')
         }}</span>
       </a>
-      <button
-        @click="showMenu = !showMenu"
-        data-collapse-toggle="navbar-hamburger"
-        type="button"
-        class="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200"
-        aria-controls="navbar-hamburger"
-        aria-expanded="false"
-      >
-        <span class="sr-only">Open main menu</span>
-        <svg
-          class="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
+      <div class="flex flex-wrap items-center justify-between gap-2">
+        <button
+          @click="showMenu = !showMenu"
+          data-collapse-toggle="navbar-hamburger"
+          type="button"
+          class="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200"
+          aria-controls="navbar-hamburger"
+          aria-expanded="false"
         >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
-      </button>
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
 
-      <div class="VPNavScreenAppearance">
-        <VTSwitchAppearance></VTSwitchAppearance>
+        <div class="VPNavScreenAppearance">
+          <VTSwitchAppearance></VTSwitchAppearance>
+        </div>
       </div>
 
       <div v-if="showMenu" class="w-full" id="navbar-hamburger">
