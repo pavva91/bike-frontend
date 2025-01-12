d5e86c7e101ec703c921a0a9d0e001e320c02db1
pavva91 <valerio.mattioli@proton.me>
2025-01-12 22:35:14 +0100

    update packages to latest versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {}
+    extend: {},
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: [require('flowbite/plugin')]
+  plugins: [require('flowbite/plugin')],
+  content: ['./node_modules/flowbite/**/*.js', 'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}'],
 }
